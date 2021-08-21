b3b325f8509a2326e0688488882bcdcce8b15d2d
JungJinwoo <48818574+Taller88@users.noreply.github.com>
2021-08-21 15:39:07 +0900

    app.js initial Commit

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+
+var app = express();
+//3000포트 기반으로 아래 콜백 함수를 실행
+app.listen(3000, function(){
+    console.log("start Server!!");
+})
+
+
+// 아래 로그가 먼저 실행된다. 이유는 비동기 
+console.log("end of server code");
+
+
+app.get('/', function (req, res){
+    res.send("<h1>HI!</h1>")
+})
