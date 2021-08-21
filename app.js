3efba04f8739e5e593c04ad2f1b66f9879352377
JungJinwoo <48818574+Taller88@users.noreply.github.com>
2021-08-21 15:52:09 +0900

    url routing Get

--- app.js
+++ app.js
@@ -6,12 +6,24 @@ app.listen(3000, function(){
     console.log("start Server!!");
 })
 
+// static 디렉터리를 express에 등록하는 과정
+// static 파일은 html, css, js 같은 정적인 파일을 의미
+app.use(express.static('public'))
+
 
 // 아래 로그가 먼저 실행된다. 이유는 비동기 
 console.log("end of server code");
 
-
+//url routing
 app.get('/', function (req, res){
     // res.send("<h1>HI!</h1>")
+    console.log("/ Function")
     res.sendFile(__dirname+"/public/main.html");
 })
+
+app.get('/main', function (req, res){
+    // res.send("<h1>HI!</h1>")
+    console.log("main Function")
+    res.sendFile(__dirname+"/public/main.html");
+})
+
